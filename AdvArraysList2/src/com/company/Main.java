86152dbe36d9078b8bdd4d0aac95ba4926f12dfa
rs5500 <rs5500@students.inghamisd.org>
2019-03-28 12:41:59 -0400

    done?

--- AdvArraysList2/src/com/company/Main.java
+++ AdvArraysList2/src/com/company/Main.java
@@ -13,14 +13,22 @@ public class Main {
             System.out.println("Would you like to (create account) or (login)?");
             String userWill = console.nextLine();
             if (userWill.equals("create account")){
+                boolean n = true;
                 System.out.println("Enter Username:");
-                String user = console.nextLine();
-                //COME BACK TO HERE.
-                System.out.println("Enter Password:");
-                String pass = console.nextLine();
-                //COME BACK
-                username.add(user);
-                password.add(pass);
+                while(n) {
+                    String user = console.nextLine();
+                    //COME BACK TO HERE.
+                    if (username.contains(user)) {
+                        System.out.println("please enter a different username");
+                    } else {
+                        System.out.println("Enter Password:");
+                        String pass = console.nextLine();
+                        //COME BACK
+                        username.add(user);
+                        password.add(pass);
+                        n = false;
+                    }
+                }
             } else if(userWill.equals("login")){
                 System.out.println("Enter Username:");
                 String user = console.nextLine();
@@ -28,7 +36,13 @@ public class Main {
                 System.out.println("Enter Password:");
                 String pass = console.nextLine();
                 //COME BACK
-                while(true)
+                for(int i = 0; i < username.size(); i++){
+                    if(username.get(i).equals(user) && password.get(i).equals(pass)){
+                        System.out.println("welcome, " + user);
+                        System.exit(0);
+                    }
+                }
+                System.out.println("sorry account does not match records");
             }
         }
     }
